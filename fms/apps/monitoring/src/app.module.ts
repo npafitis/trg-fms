285d229a0a76f6be79e52b32e498d20bc182ad87
npafitis <npafit01@cs.ucy.ac.cy>
2019-10-28 21:09:11 +0200

    Modules

--- fms/apps/monitoring/src/app.module.ts
+++ fms/apps/monitoring/src/app.module.ts
@@ -3,10 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HeartbeatModule } from './heartbeat/heartbeat.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../database/src/users/shared/user.entity';
+import { Driver } from '../../database/src/drivers/shared/driver.entity';
+import { Car } from '../../database/src/cars/shared/car.entity';
+import { Trip } from '../../database/src/trips/shared/trip.entity';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://mongo/nest'), HeartbeatModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'fms',
+      password: 'secret',
+      database: 'fms',
+      entities: [User, Driver, Car, Trip],
+      synchronize: false,
+    }),
+    MongooseModule.forRoot('mongodb://mongo/nest'),
+    HeartbeatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- fms/apps/ping/src/app.module.ts
+++ fms/apps/ping/src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HeartbeatModule } from '../../monitoring/src/heartbeat/heartbeat.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../database/src/users/shared/user.entity';
+import { Driver } from '../../database/src/drivers/shared/driver.entity';
+import { Car } from '../../database/src/cars/shared/car.entity';
+import { Trip } from '../../database/src/trips/shared/trip.entity';
 
 @Module({
-  imports: [HeartbeatModule,
+  imports: [
+    HeartbeatModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'fms',
+      password: 'secret',
+      database: 'fms',
+      entities: [User, Driver, Car, Trip],
+      synchronize: false,
+    }),
     ClientsModule.register([{ name: 'HEARTBEAT_SERVICE', transport: Transport.RMQ }])],
   controllers: [AppController],
   providers: [AppService],
